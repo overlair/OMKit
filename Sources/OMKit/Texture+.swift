46f54957b46d1282785e6e04ec02457f927b12da
overlair <overlair@gmail.com>
2024-08-26 15:53:11 -0700

    add LeftAlignedFlowLayout

--- Sources/OMKit/Texture+.swift
+++ Sources/OMKit/Texture+.swift
@@ -123,6 +123,10 @@ public extension ASCollectionNode {
 }
 
 
+
+
+
+
 import SwiftUI
 struct ASDKView<N: ASDisplayNode, T: ASDKViewController<N>>: View {
     let controller: T
@@ -143,3 +147,25 @@ struct ASDKViewControllerRepresentable<N: ASDisplayNode, T: ASDKViewController<N
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
 }
 
+
+
+
+import UIKit
+
+public class LeftAlignedFlowLayout: UICollectionViewFlowLayout{
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        var newAttributesArray = [UICollectionViewLayoutAttributes]()
+        guard let superAttributesArray = super.layoutAttributesForElements(in: rect) else { return nil }
+        for (index, attributes) in superAttributesArray.enumerated() {
+            if index == 0 || superAttributesArray[index - 1].frame.origin.y != attributes.frame.origin.y {
+                attributes.frame.origin.x = sectionInset.left
+            } else {
+                let previousAttributes = superAttributesArray[index - 1]
+                let previousFrameRight = previousAttributes.frame.origin.x + previousAttributes.frame.width
+                attributes.frame.origin.x = previousFrameRight + minimumInteritemSpacing
+            }
+            newAttributesArray.append(attributes)
+        }
+        return newAttributesArray
+    }
+}
